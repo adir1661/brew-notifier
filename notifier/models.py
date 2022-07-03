2296723218e1f7cd87145ec21188e31b35f45bbc
Adir <adir@gebrew.com>
2022-07-03 11:32:08 +0300

    models types corrected.

--- notifier/models.py
+++ notifier/models.py
@@ -9,11 +9,12 @@ class Entity:
 
 
 class CrawlableModel(models.Model, Entity):
-    uuid = models.UUIDField(default=uuid4)
-    link = models.URLField(max_length=255)
-    name = models.CharField(max_length=255)
+    link = models.URLField(max_length=255, blank=True)
+    name = models.CharField(max_length=255, blank=True)
     crawling_status = models.CharField(
-        choices=[(v.value, v.name) for v in CRAWLING_STATUSES], max_length=255
+        choices=[(v.value, v.name) for v in CRAWLING_STATUSES],
+        max_length=255,
+        default=CRAWLING_STATUSES.NOT_CRAWLED,
     )
     is_deleted = models.BooleanField(default=False)
     is_blacklisted = models.BooleanField(default=False)
@@ -24,45 +25,45 @@ class CrawlableModel(models.Model, Entity):
 
 
 class Event(CrawlableModel):
-    start_date = models.DateTimeField(null=True)
-    end_date = models.DateTimeField(null=True)
-    description = models.TextField()
-    location = models.CharField(max_length=255)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(blank=True)
+    description = models.TextField(blank=True)
+    location = models.CharField(max_length=255, blank=True)
 
 
 class Webinar(CrawlableModel):
+    link = models.URLField(max_length=255)
     start_date = models.DateTimeField()
-    description = models.TextField()
-    language = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    language = models.CharField(max_length=255, default="en")
 
     class META:
         unique_together = ("start_date", "link")
 
 
 class Company(CrawlableModel):
-    employees_min = models.IntegerField()
-    employees_max = models.IntegerField()
+    employees_min = models.PositiveIntegerField(blank=True, default=1)
+    employees_max = models.PositiveIntegerField(blank=True, default=1)
+
 
 
 class ContentItem(CrawlableModel):
     link = models.CharField(max_length=255, unique=True)
-    snippet = models.CharField(max_length=255)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    snippet = models.CharField(max_length=255, blank=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, blank=True)
 
     class META:
         unique_together = "link"
 
 
 class CompanyForEvent(models.Model, Entity):
-    uuid = models.UUIDField(default=uuid4)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    is_deleted = models.BooleanField(default=False)
-    is_blacklisted = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False, blank=True)
+    is_blacklisted = models.BooleanField(default=False, blank=True)
 
 
 class CompanyForWebinar(models.Model, Entity):
-    uuid = models.UUIDField(default=uuid4)
     webinar = models.ForeignKey(Webinar, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     is_deleted = models.BooleanField(default=False)
@@ -70,7 +71,6 @@ class CompanyForWebinar(models.Model, Entity):
 
 
 class CompanyCompetitor(models.Model, Entity):
-    uuid = models.UUIDField(default=uuid4)
     company = models.ForeignKey(
         Company, on_delete=models.CASCADE, related_name="company"
     )
