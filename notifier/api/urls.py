89e14cfb0d8a9d71ba802c2388fd700d52823acb
Adir <adir@gebrew.com>
2022-07-10 15:21:48 +0300

    completed all viewsets and urls

--- notifier/api/urls.py
+++ notifier/api/urls.py
@@ -1,8 +1,16 @@
-from notifier.api.views import EventViewSet
+from notifier.api.views import (
+    EventViewSet,
+    CompanyViewSet,
+    WebinarViewSet,
+    ContentItemViewSet,
+)
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
-router.register(r'event', EventViewSet, basename='event')
+router.register(r"event", EventViewSet, basename="Event")
+router.register(r"company", CompanyViewSet, basename="Company")
+router.register(r"contentItem", ContentItemViewSet, basename="ContentItem")
+router.register(r"webinar", WebinarViewSet, basename="Webinar")
 
 urlpatterns = router.urls
--- notifier/api/views.py
+++ notifier/api/views.py
@@ -1,8 +1,28 @@
 from rest_framework.viewsets import ModelViewSet
-from notifier.models import Event
-from notifier.api.serializers import EventSerializer
+from notifier.models import Event, Company, ContentItem, Webinar
+from notifier.api.serializers import (
+    EventSerializer,
+    CompanySerializer,
+    ContentItemSerializer,
+    WebinarSerializer,
+)
 
 
 class EventViewSet(ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+
+class CompanyViewSet(ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+
+class WebinarViewSet(ModelViewSet):
+    queryset = Webinar.objects.all()
+    serializer_class = WebinarSerializer
+
+
+class ContentItemViewSet(ModelViewSet):
+    queryset = ContentItem.objects.all()
+    serializer_class = ContentItemSerializer
